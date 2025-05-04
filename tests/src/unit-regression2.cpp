1ef0982059f63f6bfd1d181304e9419e93c70d3a
Niels Lohmann <mail@nlohmann.me>
2025-05-04 12:48:40 +0200

    :rotating_light: fix warnings
    Signed-off-by: Niels Lohmann <mail@nlohmann.me>

--- tests/src/unit-regression2.cpp
+++ tests/src/unit-regression2.cpp
@@ -919,7 +919,7 @@ TEST_CASE("regression tests 2")
 
     SECTION("issue #2982 - to_{binary format} does not provide a mechanism for specifying a custom allocator for the returned type")
     {
-        const std::vector<std::uint8_t, my_allocator<std::uint8_t>> my_vector;
+        std::vector<std::uint8_t, my_allocator<std::uint8_t>> my_vector;
         json j = {1, 2, 3, 4};
         json::to_cbor(j, my_vector);
         json k = json::from_cbor(my_vector);
